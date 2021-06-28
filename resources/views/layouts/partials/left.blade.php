9c2aa05314324bb7362a4dd04e66655b56d4c854
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-28 10:27:34 +0300

    Layout yapısı düzenlendi

--- resources/views/layouts/partials/left.blade.php
+++ resources/views/layouts/partials/left.blade.php
@@ -1,5 +1,5 @@
 <div class="list-group">
-    <a href="{{ route('home') }}" class="list-group-item list-group-item-action">Home</a>
+    <a href="{{ route('anasayfa') }}" class="list-group-item list-group-item-action">Anasayfa</a>
     <a href="#" class="list-group-item list-group-item-action">Türkçe</a>
     <a href="#" class="list-group-item list-group-item-action">İngilizce</a>
     <a href="#" class="list-group-item list-group-item-action">Almanca</a>
