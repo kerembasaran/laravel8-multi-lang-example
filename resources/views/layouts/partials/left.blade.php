69ef5c4b2cd21560710fbfb26f00b5ff431d978c
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-29 10:00:23 +0300

    Linkler düzenlendi

--- resources/views/layouts/partials/left.blade.php
+++ resources/views/layouts/partials/left.blade.php
@@ -1,7 +1,7 @@
 <div class="list-group">
-    <a href="#" class="list-group-item list-group-item-action">Anasayfa</a>
-    <a href="#" class="list-group-item list-group-item-action">Türkçe</a>
-    <a href="#" class="list-group-item list-group-item-action">İngilizce</a>
-    <a href="#" class="list-group-item list-group-item-action">Almanca</a>
+    <a href="/" class="list-group-item list-group-item-action">Anasayfa</a>
+    <a href="tr" class="list-group-item list-group-item-action">Türkçe</a>
+    <a href="en" class="list-group-item list-group-item-action">İngilizce</a>
+    <a href="de" class="list-group-item list-group-item-action">Almanca</a>
 </div>
 
